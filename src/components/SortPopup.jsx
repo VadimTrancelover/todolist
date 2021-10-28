10254fb8f88e7e1ff0aacb0c74f8c848daddca60
VadimTrancelover <fable919@gmail.com>
2021-10-28 18:53:06 +0300

    add options for filter tasks

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 
 function SortPopup() {
     return (
-        <div>
-            <ul  className="sortList">Сортировать по:
-                <li className="item1">Сортировать по:
-                    <ul className="list2" list-style="none">
-                        <li className="item2">Выполненным</li> 
-                        <li className="item2">Важным</li> 
-                    </ul> 
-                </li> 
-            </ul> 
-        </div>
+       <div>
+        <form>
+            <b> Select you favourite tutorial site using dropdown list </b>
+                <select id = "myList" onchange = "favTutorial()" >
+                    <option> ---Choose tutorial--- </option>
+                    <option> w3schools </option>
+                    <option> Javatpoint </option>
+                    <option> tutorialspoint </option>
+                    <option> geeksforgeeks </option>
+                </select>
+            <p> Your selected tutorial site is: 
+                 <input type = "text" id = "favourite" size = "20"/>
+            </p>
+        </form>
+        </div> 
     )
 }
 
