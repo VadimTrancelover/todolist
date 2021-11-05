4b46ff30dcd20f64d2d46148fc32f6cdf5f832f3
VadimTrancelover <fable919@gmail.com>
2021-11-05 16:58:11 +0300

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div>
             <div className="footer-about"> 
-                <h4>Developed by "InHomeCorporation" <a className="footer-link-developer" href="https://github.com/VadimTrancelover">Vadim Tunchik</a></h4> 
+                <h4>Developed by "InHomeCorporation" <a className="footer-link-developer" href="https://github.com/VadimTrancelover">Vadim Tunchik</a>.</h4> 
             </div>
         </div>
     )
